222df71903fdc4ba09c13f6d623f32705cfa487b
Jaein Kim <jaeinn23@gmail.com>
2023-08-12 13:48:54 +0900

    싱글톤 컨테이너

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -42,7 +44,27 @@ public class SingletonTest {
         //same : java에서 == 비교
         //equal : java equals 비교
 
-
     }
 
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+//        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+//        MemberService memberService2 = appConfig.memberService();
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //참조값이 다르게 생성되는 것을 확인 -> JVM메모리에 계속 객체가 생성되어서 올라간다
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 =/= memberService2
+        assertThat(memberService1).isSameAs(memberService2);
+    }
 }
