518787060ff27e18a92dc68667b2988589083677
Jaein Kim <jaeinn23@gmail.com>
2023-08-13 13:57:22 +0900

    탐색 위치와 기본 스캔 대상

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(  //@Component 붙은 클래스를 찾아서 다 자동으로 스프링 빈으로 등록해줌
+        basePackages = "hello.core", // hello.core이 위치에서부터 시작해서 하위 패키지를 찾아간다
+        basePackageClasses = AutoAppConfig.class,
+
+        //탐색위치를 지정하지 않으면? @ComponentScan이 붙은 클래스가 디폴트
+        //지금 이 코드 기준으로는, hello.core 패키지에서부터 시작해서 그 패키지를 포함하여 하위패키지까지 다 찾는다
+
         excludeFilters =  @ComponentScan.Filter(type =  FilterType.ANNOTATION, classes = Configuration.class)
         //그중에서 뺄 것들을 지정 (Configuration 어노테이션이 붙은 것들은 빼겠다!)
         //Configuration를 빼놓는 이유는 AppConfig클래스 때문임
