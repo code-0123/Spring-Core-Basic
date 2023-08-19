26bc6c261b616f44f07dcabe94853e69087f4493
Jaein Kim <jaeinn23@gmail.com>
2023-08-19 20:34:13 +0900

    섹션 9. 빈 스코프 : 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제 해결

--- src/test/java/hello/core/scope/SingletonWithProtoTypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithProtoTypeTest1.java
@@ -2,12 +2,14 @@ package hello.core.scope;
 
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -40,41 +42,23 @@ public class SingletonWithProtoTypeTest1 {
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
 
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean; //생성시점에 주입 x01
 
-        @Autowired //autowired는 생략 가능
-        public ClientBean(PrototypeBean prototypeBean){
-            this.prototypeBean = prototypeBean;
-        }
+        @Autowired //테스트니까 그냥 필드주입으로..
+        private Provider<PrototypeBean> prototypeBeanProvider; //ObjectProvider를 ObjectFactory로 바꿔도 된다
 
         public int logic(){
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
         }
     }
-
-/*    @Scope("singleton")
-    static class ClientBean2{
-        private final PrototypeBean prototypeBean; //생성시점에 주입 x02
-
-        @Autowired //autowired는 생략 가능
-        public ClientBean2(PrototypeBean prototypeBean){
-            this.prototypeBean = prototypeBean;
-        }
-
-        public int logic(){
-            prototypeBean.addCount();
-            int count = prototypeBean.getCount();
-            return count;
-        }
-    }*/
 
     @Scope("prototype")
     static class PrototypeBean {
